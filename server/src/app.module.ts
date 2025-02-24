0a8ed8baaa2b9c9c1d6e8117a9b233ac90f75368
p-svacha <philip.svacha@gmail.com>
2025-02-24 15:26:43 +0100

    fix entity path, update bruno requests

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -27,7 +27,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity.ts'],
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
       synchronize: true,
     }),
     TournamentModule
