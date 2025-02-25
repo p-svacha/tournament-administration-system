ba54399b5622e37e961c34be058b8267ed76ffed
p-svacha <philip.svacha@gmail.com>
2025-02-25 18:09:36 +0100

    fix missing module imports, update bruno requests

--- server/src/tournament/tournament.module.ts
+++ server/src/tournament/tournament.module.ts
@@ -3,9 +3,10 @@ import { TournamentService } from './tournament.service';
 import { TournamentResolver } from './tournament.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TournamentEntity } from './tournament.entity';
+import { TournamentParticipantModule } from 'src/tournament-participant/tournament-participant.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TournamentEntity])],
+  imports: [TypeOrmModule.forFeature([TournamentEntity]), TournamentParticipantModule],
   providers: [TournamentService, TournamentResolver],
   exports: [TournamentService]
 })
--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { TournamentParticipantModule } from 'src/tournament-participant/tournament-participant.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), TournamentParticipantModule],
   providers: [UserService, UserResolver],
   exports: [UserService]
 })
