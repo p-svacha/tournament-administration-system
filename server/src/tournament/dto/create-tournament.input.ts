b44873af5c73a0545608378de57d790f923b8524
Maxasi <ma.si@bluemail.ch>
2025-03-16 20:22:38 +0100

    updated and added GraphQL queries

--- server/src/tournament/dto/create-tournament.input.ts
+++ server/src/tournament/dto/create-tournament.input.ts
@@ -1,4 +1,4 @@
-import { InputType, Field, Int } from '@nestjs/graphql';
+import {Field, InputType, Int} from '@nestjs/graphql';
 
 /**
  * Data that needs to be provided to the API to create a new tournament.
@@ -13,4 +13,40 @@ export class CreateTournamentInput {
 
   @Field(() => Int)
   gameId: number;
+
+  @Field()
+  category?: string;
+
+  @Field()
+  registrationGroup?: string;
+
+  @Field()
+  rules?: string;
+
+  @Field()
+  prize1: string;
+
+  @Field()
+  prize2: string;
+
+  @Field()
+  prize3: string;
+
+  @Field()
+  numPlayersPerTeam: number;
+
+  @Field()
+  maxSubstitutes: number;
+
+  @Field()
+  minParticipants?: number;
+
+  @Field()
+  maxParticipants?: number;
+
+  @Field()
+  briefingTime?: Date;
+
+  @Field()
+  isPublished: boolean;
 }
